a71490c8ee339fd196381524d82a4c87212e6524
Alibek <freed.alibek@gmail.com>
2024-10-18 23:10:25 +0800

    ihatewindows

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,8 @@ import WebApp from "@twa-dev/sdk";
 
 import { ThemeProvider } from "@/providers/theme";
 
-import { Main } from "@/pages/main";
-import { Profile } from "@/pages/profile";
+import { Main } from "@/pages/Main1";
+import { Profile } from "@/pages/Profile1";
 
 import "./index.css";
 
