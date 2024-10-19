b0e3d0c6eaf160768ac50a8e6aa302d15f2bd2fb
Alibek <freed.alibek@gmail.com>
2024-10-19 16:37:13 +0800

    adding user into app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 
 import { PostModule } from "@/modules/post/post.module";
+import { UserModule } from "./modules/user/user.module";
 
 @Module({
-  imports: [PostModule],
+  imports: [PostModule, UserModule],
 })
 export class AppModule {}
